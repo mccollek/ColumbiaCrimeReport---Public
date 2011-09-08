9cf9f5446d900ade20e4b7b590783bc042cc3f59
Karl McCollester <karlmccollester@udhaa.com>
2011-09-07 22:39:24 -0400

    Added GPL Copyright language

--- src/com/udhaa/ColumbiaCrimeReport/ColumbiaCrimeReportActivity.java
+++ src/com/udhaa/ColumbiaCrimeReport/ColumbiaCrimeReportActivity.java
@@ -1,5 +1,25 @@
 package com.udhaa.ColumbiaCrimeReport;
 
+//ColumbiaCrimeReport is an Android application written in Java
+//Copyright 2011 Karl McCollester and Palmetto Computer Labs, LLC
+//Licensed under the Gnu GPL
+//
+//    This file is part of the Columbia Crime Report (iCrime) software.
+//
+//    The Columbia Crime Report (iCrime) software is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    The Columbia Crime Report (iCrime) software is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import java.io.IOException;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
